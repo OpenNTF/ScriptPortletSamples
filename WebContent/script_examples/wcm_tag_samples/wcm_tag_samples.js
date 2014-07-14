c58f805d1ebd780ba6c26efda4baf8d557980f3d
jbooth1 <jbooth1@gmail.com>
2014-07-14 17:08:46 -0400

    Added wcm_tag_samples

---
+++ WebContent/script_examples/wcm_tag_samples/wcm_tag_samples.js
@@ -0,0 +1,35 @@
+
+/*
+ * Copyright 2014  IBM Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except irani n compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+*/
+function displayDataTable(data, tableId) {
+    var items = [];   
+
+    $.each( data, function( index, rowData) {      
+  	  var data ='<tr>';
+  	  // add columns
+  	      $.each( rowData, function( index2, columnData) {      
+  	        data = data + '  <td>' + columnData + '</td>';
+	   	console.log("columnData: " + columnData);
+	      });   
+
+	  data = data + '</tr>';
+ 	console.log("rowData: " + rowData);
+        items.push( data );
+    });   
+      
+    $('#' + tableId).append("<tbody></tbody>").append(items); 
+
+  };
+  
+//Function called by jQuery when DOM has finished loading - display list of customers
+$(document).ready(function () {
+  displayDataTable(wcmTagsSampleData, "tags_table"); 
+  
+});
