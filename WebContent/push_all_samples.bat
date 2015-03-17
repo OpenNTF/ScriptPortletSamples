1eab2f86f4d7e6aafacd1067eb29426f96a342bd
gsager <gsager@us.ibm.com>
2015-03-17 16:40:50 -0400

    Updates to doc

--- WebContent/push_all_samples.bat
+++ WebContent/push_all_samples.bat
@@ -4,7 +4,7 @@ FOR /D %%f IN (*) DO (
     IF EXIST index.html (
 
 	echo pushing %%f
-	rem set path to sp.bat in the following line or add that path to your enviorment variable:	
+	rem set path to sp.bat in the following line or add that path to your environment variable:	
 	call c:\files\script_portlet\1.3\sp_cmdln\sp push
     )
     cd ..
